ed3d5383b3b48feda703f1c140133b3fae24e696
siesto1elemento <rohitojha9720@gmail.com>
2025-03-16 17:28:16 +0530

    Minor fixes as devfolio changed html class names

--- scraping/utils.py
+++ scraping/utils.py
@@ -8,20 +8,23 @@ def hackathon_():
     
     soup = BeautifulSoup(html, 'html.parser')
 
-    hackathons = soup.find_all('div', class_='sc-dwVMhp fdwmgb CompactHackathonCard__StyledCard-sc-4a10fa2a-0 ihpCnk')
+    hackathons = soup.find_all('div', class_='sc-fmGnzW iXcaew CompactHackathonCard__StyledCard-sc-9ff45231-0 fudhHJ')
     hackathon_data = []
     for hackathon in hackathons:
-        heading = hackathon.find('h3', class_='sc-cHPgQl guEEhq').text
-        theme = hackathon.find('p', class_='sc-cHPgQl kCrHaC').text
-        theme_result = hackathon.find('div', class_='sc-tQuYZ hdPpzk').text
-        link = hackathon.find('a', class_='Link__LinkBase-sc-af40de1d-0 lkflLS').get('href')
+        heading = hackathon.find('h3', class_='sc-hZgfyJ oSdsf')
+        theme = hackathon.find('p', class_='sc-hZgfyJ hZQPen')
+        theme_result = hackathon.find('div', class_='sc-hZgfyJ hZQPen')
+
+        heading = heading.text if heading else ""
+        theme = theme.text if theme else ""
+        theme_result = theme_result.text if theme_result else ""
+
         
 
         hackathon_data.append({
             'heading': heading,
             'theme': theme,
             'theme_result': theme_result,
-            'link':link,
         })
     
     
